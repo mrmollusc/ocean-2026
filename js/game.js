4806c59343e331efdaf88a840e367a2bc91044ca
Basiliskk0 <aaron.zhu1@education.nsw.gov.au>
2026-07-30 21:04:20 +1000

    changed iframe again

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@
 
   //player health and damage
   const PLAYER_MAX_HEALTH = 100;
-  const PLAYER_IFRAME_DURATION = 200;
+  const PLAYER_IFRAME_DURATION = 100;
 
   //trash damage
   const TRASH_DAMAGE = 1;
